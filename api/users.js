e3ed56bbe640fbbfc5566dcbdf4fd3f40aa9fb3d
TaylorEdelman <Taylor.edelman17@gmail.com>
2022-07-22 14:07:28 -0500

    req.body giving undefined

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
 
-const { getAllUsers } = require("../db");
+const { getAllUsers, getUserByUsername } = require("../db");
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
@@ -10,6 +10,7 @@ usersRouter.use((req, res, next) => {
 
 usersRouter.get("/", async (req, res) => {
   const users = await getAllUsers();
+  console.log(users);
 
   res.send({
     users,
@@ -17,8 +18,32 @@ usersRouter.get("/", async (req, res) => {
 });
 
 usersRouter.post("/login", async (req, res, next) => {
-  console.log(req.body);
-  res.end();
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
